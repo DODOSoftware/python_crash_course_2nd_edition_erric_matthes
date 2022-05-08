fcc7f7c8b96d5819ff20d00c22985ca2ba442221
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-08 08:34:58 +0200

    Add files via upload

---
+++ exercise2-7-2_removing_whitespace.py
@@ -0,0 +1,9 @@
+# .rstrip() .lstrip() .strip()
+
+fav_lang = '                    python          '
+
+fav_lang = fav_lang.strip()
+print ( fav_lang )
+
+message = fav_lang.strip()
+print ( message )
