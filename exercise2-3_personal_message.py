73fa2bc969fca5699e68152b504b0d3d549297d5
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-10 15:14:08 +0200

    Delete exercise2-3_personal_message.py

--- exercise2-3_personal_message.py
+++
@@ -1,5 +0,0 @@
-fst_name = "eric"
-lst_name = "kraken"
-full_name = f"{ fst_name } { lst_name }"
-greet_msg = f"Hello { full_name.title() }, would you like to learn Python today?"
-print ( greet_msg )
