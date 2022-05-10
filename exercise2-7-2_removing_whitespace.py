8630a690cc73350d2afebae0d24c820e4a1fbc41
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-10 15:14:46 +0200

    Delete exercise2-7-2_removing_whitespace.py

--- exercise2-7-2_removing_whitespace.py
+++
@@ -1,9 +0,0 @@
-# .rstrip() .lstrip() .strip()
-
-fav_lang = '                    python          '
-
-fav_lang = fav_lang.strip()
-print ( fav_lang )
-
-message = fav_lang.strip()
-print ( message )
