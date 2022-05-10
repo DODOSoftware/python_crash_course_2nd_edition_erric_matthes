9f10106e17c3af07cc59b9774f43f2b09e7912da
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-10 11:41:50 +0200

    Add files via upload

--- exercise3-8_seeing_the_world.py
+++ exercise3-8_seeing_the_world.py
@@ -1,3 +1,31 @@
+places = [ 'norway', 'usa', 'san giovani rotondo italy', 'canada', 'japan', 'thailand', 'bora-bora', 'the maldives', 'philiphines', 'greece' ]
+print ( f'Places we want to go:' )
+print ( places )
+
+print ( '\nUse .sorted() method:')
+print ( sorted( places ) )
+
+print ( '\nStill intact, because of .sorted() method:')
+print ( places )
+
+print ( '\nReverse and then print:' )
+places.reverse()
+print ( places )
+
+print ( '\nAnd reverse again:' )
+places.reverse()
+print ( places )
+
+print ( '\nNow .sort() and then print:' )
+places.sort()
+print ( places )
+
+print ( '\nAnd at last .sort() in reverse:' )
+places.sort( reverse = True )
+print ( places )
+
+
+
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 print ( '\n\n' )
