dece8f643ea0891320490733493c46a84d108fad
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-10 15:13:15 +0200

    Delete exercise1-3_infinite_skills.py

--- exercise1-3_infinite_skills.py
+++
@@ -1,8 +0,0 @@
-first_app = "1. Gym/Masher. Online app for making grind you training harder!"
-print ( first_app )
-
-second_app = "2. Providing your data presentation in niiiice circles."
-print ( second_app )
-
-third_app = "3. I DON'T FUKCING KNOW YET"
-print ( third_app )
