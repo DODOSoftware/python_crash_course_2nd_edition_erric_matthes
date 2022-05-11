47b72bfe540caeed50a46867fb25e698cc742756
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-11 15:55:22 +0200

    Add files via upload

---
+++ exercise_004-04-05_one_million.py
@@ -0,0 +1,15 @@
+# Exercise 004-04 One Million: 
+
+million = list( range( 1, 1000001 ) ) # one million gone good, but beyond hanged my system for a while :D
+print ( million )
+
+# Exercise 004-05 Summing a million:
+
+minimum = min( million )
+print ( minimum )
+
+maximum = max( million )
+print ( maximum )
+
+summary = sum( million )
+print ( summary )
