2e1a58f418fe89d470766fe9938a5873b7f3428e
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-11 10:04:03 +0200

    Add files via upload

---
+++ exercise_004-02_animals.py
@@ -0,0 +1,6 @@
+pet_animals = [ 'dogs', 'cats', 'rabbits', 'rats' ]
+
+for pet_animal in pet_animals:
+	print ( f'{ pet_animal.title() } are great home pets.' )
+
+print ( '\nAll those animals would be a great home pets!' )
