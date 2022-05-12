0773d4ebc537bb4e37bb2b445875f6eb2ece9c83
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-12 09:51:17 +0200

    Add files via upload

--- exercise_004-10_slices.py
+++ exercise_004-10_slices.py
@@ -1,8 +1,8 @@
-# Exercise 004-10 Slices:
+# Exercise 004-10 Slices: The output retain the structure of the list and includes [ pointed ] items.
 
 items = ['item00', 'item01', 'items03', 'item04', 'item05', 'items06', 'item07', 'item08', 'item09']
 
-print ('First three elements in the items list are:')
+print ('\nFirst three elements in the items list are:')
 print (items[0:3])
 
 print ('\nThree elements in the middle of the items list are:')
