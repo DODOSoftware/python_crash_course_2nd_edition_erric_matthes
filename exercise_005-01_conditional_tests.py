2233e563047ac3a7b5a8cf54bf7f90a4422823e9
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-14 13:42:39 +0200

    Add files via upload

--- exercise_005-01_conditional_tests.py
+++ exercise_005-01_conditional_tests.py
@@ -1,4 +1,109 @@
-# Exercise 005-01 Conditional testing
+# Exercise 005-01 Conditional testing - write a series of conditional tests:
+car = 'subaru'
+if car == 'subaru':
+    print(f'If car is {car.title()} I predict True!')
+if car == 'toyota':
+    print(f'\nIf car is a {car.title()} I predict False!')
+    # Paul 14.05.2022 no print for you Toyota, sorry.
+
+swimmingpool = ['towel', 'flip-flops', 'cap', 'goggles', 'trunks']
+
+print('\n== True with "for" loop:') # Paul 14.05.2022 == True
+for swim_item0 in swimmingpool:
+    if swim_item0 == 'towel':
+        print(f'We have {swim_item0}.')
+for swim_item1 in swimmingpool:
+    if swim_item1 == 'flip-flops':
+        print(f'We have {swim_item1}.')
+for swim_item2 in swimmingpool:
+    if swim_item2 == 'cap':
+        print(f'We have {swim_item2}.')
+for swim_item3 in swimmingpool:
+    if swim_item3 == 'goggles':
+        print(f'We have {swim_item3}.')
+for swim_item4 in swimmingpool:
+    if swim_item4 == 'trunks':
+        print(f'We have {swim_item4}.')
+
+print('\n== False with "for" loop:') # Paul 14.05.2022 == False
+for swim_item0 in swimmingpool:
+    if swim_item0 == 'soap':
+        print(f'We have {swim_item0}.')
+for swim_item1 in swimmingpool:
+    if swim_item1 == 'mattress':
+        print(f'We have {swim_item1}.')
+for swim_item2 in swimmingpool:
+    if swim_item2 == 'swimming sleeves':
+        print(f'We have {swim_item2}.')
+for swim_item3 in swimmingpool:
+    if swim_item3 == 'diving mask':
+        print(f'We have {swim_item3}.')
+for swim_item4 in swimmingpool:
+    if swim_item4 == 'fins':
+        print(f'We have {swim_item4}.')
+
+print('\n!= False without loop:') # Paul 14.05.2022 != False
+swim_item5 = 'soap'
+if swim_item5 != 'soap':
+    print('haha False')
+swim_item6 = 'mattress'
+if swim_item6 != 'mattress':
+    print('haha False')
+swim_item7 = 'swimming sleeves'
+if swim_item7 != 'swimming sleeves':
+    print('haha False')
+swim_item8 = 'diving mask'
+if swim_item8 != 'diving mask':
+    print('haha False')
+swim_item9 = 'fins'
+if swim_item9 != 'fins':
+    print('haha False')
+
+print('\n!= True without loop:') # Paul 14.05.2022 != True
+swim_item5 = 'soap'
+if swim_item5 != 'cake':
+    print("haha it's not a cake!")
+swim_item6 = 'mattress'
+if swim_item6 != 'lamp':
+    print("haha it's not a lamp!")
+swim_item7 = 'swimming sleeves'
+if swim_item7 != 'painting':
+    print("haha it's not a painting!")
+swim_item8 = 'diving mask'
+if swim_item8 != 'candle':
+    print("haha it's not a candle!")
+swim_item9 = 'fins'
+if swim_item9 != 'book':
+    print("haha it's not a book!")
+
+print('\nin True:') # Paul 15.05.2022 in True:
+if 'towel'in swimmingpool:
+    print(f'We have towel.')
+if 'flip-flops':
+    print(f'We have flip-flops.')
+if 'cap' in swimmingpool:
+    print(f'We have cap.')
+if 'goggles' in swimmingpool:
+    print(f'We have goggles.')
+if 'trunks' in swimmingpool:
+    print(f'We have trunks.')
+
+print('\nnot in True:') # Paul 14.05.2022 not in True:
+swim_item10 = 'soap'
+if swim_item10 not in swimmingpool:
+    print(f"We don't have {swim_item10}!")
+swim_item11 = 'mattress'
+if swim_item11 not in 'mattress':
+    print(f"We don't have {swim_item11}!")
+swim_item12 = 'swimming sleeves'
+if swim_item12 == 'swimming sleeves':
+    print(f"We don't have {swim_item12}!")
+swim_item13 = 'diving mask'
+if swim_item13 == 'diving mask':
+    print(f"We don't have {swim_item13}!")
+swim_item14 = 'fins'
+if swim_item14 == 'fins':
+    print(f"We don't have {swim_item14}!")
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
@@ -22,7 +127,7 @@
 
 '''
 
-print('\nSimple "if" (True/False) in the loop "for":')
+print('\n\nSimple "if" (True/False) in the loop "for":')
 
 cars = ['audi', 'bmw', 'subaru', 'toyota', 'skoda']
 for car in cars:
@@ -43,7 +148,7 @@ print('\nSimple equality ==, and inequality != tests:')
 car = 'bmw'
 if car == 'bmw':
     print("\nYes, it's BMW.")
-# Paul 13.05.2022 figured it out myself :) Conditional test return True
+    # Paul 13.05.2022 figured it out myself :) Conditional test return True
 
 car = 'audi'
 if car == 'bmw':
@@ -59,7 +164,7 @@ to the value of interest.'''
 car = 'Audi'
 if car == 'audi':
     print('\nTrue!')
-# Paul 13.05.2022 testing for equality is cAsE sensitive in Python.
+    # Paul 13.05.2022 testing for equality is cAsE sensitive in Python.
 
 car = 'Audi'
 if car.lower() == 'audi':
@@ -115,3 +220,38 @@ age_1 = 17
 if (age_0 >= 21) and (age_1 >= 21):
     print('Both are age 21 or more.') # one of them it's not over 21.
 
+print('\nChecking multipe conditions with "or":')
+# Paul 14.05.2022 True if either or both of tests passes
+
+age_0 = 22
+age_1 = 18
+if (age_0 >= 21) or (age_1 >= 21):
+    print('"or" test passed.')
+
+age_0 = 20
+age_1 = 18
+if (age_0 >= 21) or (age_1 >= 21):
+    print('"or" test passed.')
+    # Paul 14.05.2022 - both are not >= 21, test returns False.
+
+print('\nChecking whether a value is IN the list:')
+required_toppings = ['mushrooms', 'onions', 'ham', 'olives', 'corn']
+if 'mushrooms' in required_toppings:
+    print('There are mushrooms in the order.')
+if 'pepperoni' in required_toppings:
+    print('There is pepperoni in the order.')
+    # Paul 14.05.2022 test returns False, so no pepperoni for you!
+
+'''NOTE form the manual: This technique is powerful because you can create
+a list of essential values, and then easily check whether the value you're
+testing matches on the values in the list.'''
+
+print('\nList of banned people: (not in checking):')
+banned_ppl = ['ewa', 'pawel', 'alina', 'mirela', 'maciej', 'jakub', 'bogna']
+user = 'hanna'
+if user not in banned_ppl:
+    print(f"{user.title()} you can post answer now.")
+
+'''Boolean expression = conditional test. True of False.
+Boolean values provide an efficient way to track the state of program or
+a particular condition that is important to you program.'''
