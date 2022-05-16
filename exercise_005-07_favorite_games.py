a2b6ee0427d73d7099f4405911a3f63aeb5a89f2
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-16 18:54:42 +0200

    Add files via upload

---
+++ exercise_005-07_favorite_games.py
@@ -0,0 +1,59 @@
+# Exercise 005-07 Favorite games:
+
+games_list = []
+
+games_list.append('Doom')
+games_list.append('Warcraft II')
+games_list.append('Duke Nukem')
+games_list.append('TekWar')
+games_list.append('Worms')
+games_list.append('Quake')
+games_list.append('Super Mario Bros')
+games_list.append('Another World')
+games_list.append('Quake 2')
+games_list.append('Aliens vs Predator')
+games_list.append('Half-Life')
+games_list.append('Fallout 2')
+games_list.append('Heroes of Might and Magic III')
+games_list.append('Half-Life 2')
+games_list.append('Jedi Knight')
+games_list.append('Rune')
+games_list.append('Warcraft III')
+games_list.append('Fallout: New Vegas')
+games_list.append('Alien: Isolation')
+games_list.append('The Last of Us')
+games_list.append('God of War IV')
+
+tlou_game = 'The Last of Us'
+if tlou_game in games_list:
+    print(f"I just love {tlou_game}! Best game ever!")
+
+alien_game = 'Alien: Isolation'
+if alien_game in games_list:
+    print(f"Another extreme good game is {alien_game}!")
+
+another_game = 'Another World'
+if another_game in games_list:
+    print(f"Ah... {another_game}... This one is super nostalgic.")
+
+fallout_game = 'Fallout 2'
+if fallout_game in games_list:
+    print(f"{fallout_game} took hours, weeks and months from my life!")
+
+half_game = 'Half-Life'
+if half_game in games_list:
+    print(f"{half_game} is on of the best science-fiction oldschool FPS! Ahhh those vibes!")
+
+fifa_game = "Fifa 11"
+if fifa_game in games_list:
+    print("Ahhh great game!")
+else:
+    print("Yeah like I care about Fifa and such games - it's not on my list.")
+
+warcraft_game = 'Warcraft II'
+if warcraft_game in games_list:
+    print("Zug zug!")
+
+heroes_game = 'Heroes of Might and Magic III'
+if heroes_game in games_list:
+    print(f"Rampart was my favorite in {heroes_game}!")
