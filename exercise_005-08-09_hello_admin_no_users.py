99d647e280ffe463f2b9ed30a5b0a537c140a5d1
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-19 09:21:19 +0200

    Add files via upload

--- exercise_005-08-09_hello_admin_no_users.py
+++ exercise_005-08-09_hello_admin_no_users.py
@@ -10,10 +10,10 @@ registered_users.append('Tygrys')
 registered_users.append('Rapek')
 registered_users.append('Kofi')
 
-for user_name in registered_users:
-    if user_name.lower() == 'admin':
-        print("Hello admin, would you like to see a status report?")
+for registered_user in registered_users:
+    if registered_user.lower() == 'admin':
+        print("Hello Admin, would you like to see a status report?")
     else:
-        print(f"Hello {user_name.title()}, nice to see you again!")
+        print(f"Hello {registered_user.title()}, nice to see you again!")
 
 # Exercise 005-09 No users:
