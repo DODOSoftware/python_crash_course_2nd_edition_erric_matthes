f0f1b9a2f7100f659a52ec55965a49f2d1aecad9
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-19 10:08:57 +0200

    Add files via upload

--- exercise_005-08-09_hello_admin_no_users.py
+++ exercise_005-08-09_hello_admin_no_users.py
@@ -17,3 +17,22 @@ for registered_user in registered_users:
         print(f"Hello {registered_user.title()}, nice to see you again!")
 
 # Exercise 005-09 No users:
+
+print("\nChecking if list is empty:")
+'''Paul 19.05.2022 again variables need to be defined realier in the code! '''
+
+
+
+
+''' SOMETHING'S NOT WORKING HERE!
+
+registered_users = []
+
+if registered_user in registered_users:
+    for registered_user in registered_users:
+        if registered_user.lower() == 'admin':
+            print('Hello Admin, would you like to see a status reports?')
+        else:
+            print(f"Hello {registered_user.title()}, nice to see you again!")
+else:
+    print("There are no registered users!")
