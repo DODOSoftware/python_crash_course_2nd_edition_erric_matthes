a09aaf1fc62f2c86526ed143b57b86e9b8326261
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-20 17:38:54 +0200

    Add files via upload

--- exercise_005-08-09_hello_admin_no_users.py
+++ exercise_005-08-09_hello_admin_no_users.py
@@ -1,38 +1,33 @@
 # Exercise 005-08 Hello Admin:
 
-registered_users = []
+users = []
 
-registered_users.append('Dodo')
-registered_users.append('admin')
-registered_users.append('Hania')
-registered_users.append('Pawel')
-registered_users.append('Tygrys')
-registered_users.append('Rapek')
-registered_users.append('Kofi')
+users.append('Dodo')
+users.append('admin')
+users.append('Hania')
+users.append('Pawel')
+users.append('Tygrys')
+users.append('Rapek')
+users.append('Kofi')
 
-for registered_user in registered_users:
-    if registered_user.lower() == 'admin':
+for user in users:
+    if user.lower() == 'admin':
         print("Hello Admin, would you like to see a status report?")
     else:
-        print(f"Hello {registered_user.title()}, nice to see you again!")
+        print(f"Hello {user.title()}, nice to see you again!")
 
 # Exercise 005-09 No users:
 
 print("\nChecking if list is empty:")
 '''Paul 19.05.2022 again variables need to be defined realier in the code! '''
 
+users = []
 
-
-
-''' SOMETHING'S NOT WORKING HERE! 
-
-registered_users = []
-
-if registered_user in registered_users:
-    for registered_user in registered_users:
-        if registered_user.lower() == 'admin':
+if users is not None:
+    for user in users:
+        if user.lower() == 'admin':
             print('Hello Admin, would you like to see a status reports?')
         else:
-            print(f"Hello {registered_user.title()}, nice to see you again!")
+            print(f"Hello {user.title()}, nice to see you again!")
 else:
     print("There are no registered users!")
