6d33a6308d73fd5218aa8a30d6cd27a4054541d0
DODOSoftware <100480430+DODOSoftware@users.noreply.github.com>
2022-05-21 08:01:17 +0200

    Add files via upload

--- chapter_006_notes_01_dictionairy.py
+++ chapter_006_notes_01_dictionairy.py
@@ -25,3 +25,18 @@ print(alien_2['points'])'''
 
 new_points = alien_0['points']
 print(f"You've earned {new_points} points!")
+
+
+# Paul 21.05.2022 inverting key-value:
+
+my_map = {'a': 0, 'b': 1, 'c': 2}
+inv_map = {v: k for k, v in my_map.items()}
+
+print(inv_map[0])
+print(my_map['a'])
+
+# Paul 21.05.2022 if values are unique
+dict((v, k) for k, v in my_map.items())
+
+'''I begin being tired of drag'n'drop to the github, I sense learning git
+commands in the near future...'''
